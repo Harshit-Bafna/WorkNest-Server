3cc9ae7ac9763e1d29d3227d61136afc85b43459
Harshit-Bafna <bafnaharshit7891@gmail.com>
2025-01-08 01:46:15 +0530

    fix: Success false message

--- server/src/router/s3FileHandler.ts
+++ server/src/router/s3FileHandler.ts
@@ -23,7 +23,7 @@ router.post('/upload', multerService.single('file'), async (req: Request, res: R
 
         const uploadData = await uploadToAWS(uploadParams)
         if (!uploadData.success) {
-            return ApiError(next, uploadData.message, req, uploadData.status)
+            return ApiError(next, null, req, uploadData.status, uploadData.message)
         }
         return ApiResponse(req, res, uploadData.status, uploadData.message, uploadData.data)
     } catch (err) {
@@ -43,7 +43,7 @@ router.get('/getSignedUrl/', async (req: Request, res: Response, next: NextFunct
         const key = req.query.key as string
         const signedUrlData = await getSignedFileURL(key)
         if (!signedUrlData.success) {
-            return ApiError(next, signedUrlData.message, req, signedUrlData.status)
+            return ApiError(next, null, req, signedUrlData.status, signedUrlData.message)
         }
         return ApiResponse(req, res, signedUrlData.status, signedUrlData.message, signedUrlData.data)
     } catch (error) {
@@ -63,7 +63,7 @@ router.delete('/deleteFile/', async (req: Request, res: Response, next: NextFunc
         const key = req.query.key as string
         const deleteData = await deleteFilefromAWS(key)
         if (!deleteData.success) {
-            return ApiError(next, deleteData.message, req, deleteData.status)
+            return ApiError(next, null, req, deleteData.status, deleteData.message)
         }
         return ApiResponse(req, res, deleteData.status, deleteData.message, deleteData.data)
     } catch (error) {
