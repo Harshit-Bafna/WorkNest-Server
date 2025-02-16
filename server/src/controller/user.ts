4672621a5622160000aefd874b0b2b212b278992
Harshit-Bafna <bafnaharshit7891@gmail.com>
2025-02-16 17:55:41 +0530

    feat: API to get user identity

--- server/src/controller/user.ts
+++ server/src/controller/user.ts
@@ -1,8 +1,11 @@
+import config from '../config/config'
 import { EUserRole } from '../constants/Enums/applicationEnums'
 import responseMessage from '../constants/responseMessage'
 import organisationModel from '../model/user/organisationModel'
 import userModel from '../model/user/userModel'
+import { IDecryptedJwt } from '../types/userTypes'
 import { ApiMessage } from '../utils/ApiMessage'
+import { VerifyToken } from '../utils/helper/syncHelpers'
 
 export const GetAllUser = async (userId: string, page: number, limit: number, search: string | null): Promise<ApiMessage> => {
     const skip = (page - 1) * limit
@@ -58,7 +61,41 @@ export const GetAllUser = async (userId: string, page: number, limit: number, se
         }
     } catch (error) {
         const errMessage = error instanceof Error ? error.message : responseMessage.SOMETHING_WENT_WRONG
+        return {
+            success: false,
+            status: 500,
+            message: errMessage,
+            data: null
+        }
+    }
+}
 
+export const SelfIdentification = async (accessToken: string): Promise<ApiMessage> => {
+    try {
+        const { userId } = VerifyToken(accessToken, config.ACCESS_TOKEN.SECRET as string) as IDecryptedJwt
+
+        const user = await userModel.findById(userId)
+        if (!user) {
+            return {
+                success: false,
+                status: 400,
+                message: responseMessage.INVALID_TOKEN,
+                data: null
+            }
+        }
+
+        return {
+            success: true,
+            status: 200,
+            message: responseMessage.SUCCESS,
+            data: {
+                userId: user._id,
+                name: user.name,
+                emailAddress: user.emailAddress
+            }
+        }
+    } catch (error) {
+        const errMessage = error instanceof Error ? error.message : responseMessage.SOMETHING_WENT_WRONG
         return {
             success: false,
             status: 500,
--- server/src/router/user.ts
+++ server/src/router/user.ts
@@ -6,7 +6,7 @@ import responseMessage from '../constants/responseMessage'
 import { VerifyToken } from '../utils/helper/syncHelpers'
 import config from '../config/config'
 import { IDecryptedJwt } from '../types/userTypes'
-import { GetAllUser, GetUserDetails } from '../controller/user'
+import { GetAllUser, GetUserDetails, SelfIdentification } from '../controller/user'
 const router = Router()
 
 /*
@@ -38,6 +38,31 @@ router.get('/allUsers', rateLimit, async (req: Request, res: Response, next: Nex
         const search: string = req.query.search as string
 
         const { success, status, message, data } = await GetAllUser(userId, page, limit, search)
+        if (!success) {
+            return ApiError(next, null, req, status, message)
+        }
+        return ApiResponse(req, res, status, message, data)
+    } catch (err) {
+        return ApiError(next, err, req, 500)
+    }
+})
+
+/*
+    Route: /api/v1/user/self-identification
+    Method: GET
+    Desc: Get user identity
+    Access: Protected
+    Query: userId
+*/
+router.get('/self-identification', rateLimit, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { cookies } = req
+        const { accessToken } = cookies as { accessToken: string | undefined }
+        if (!accessToken) {
+            return ApiError(next, null, req, 400, responseMessage.UNAUTHORIZED)
+        }
+
+        const { success, status, message, data } = await SelfIdentification(accessToken)
         if (!success) {
             return ApiError(next, null, req, status, message)
         }
